24f97dcf08e3650b5bfe599e866c19de822991c0
Andria Pratama <andrilocong1st@gmail.com>
2022-05-12 10:29:48 +0800

    make navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import Navbar from "./components/navbar";
+
 function App() {
-  return <div className="App"></div>;
+	return (
+		<div className="App">
+			<Navbar />
+
+			<BrowserRouter>
+				<Routes>
+					<Route />
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,29 @@
+import profil from "../img/photo-1622548331053-105252394.jpeg";
+
+function Navbar() {
+	return (
+		<nav className="flex h-[60px] w-full items-center justify-between border-b border-slate-200 shadow-md shadow-slate-100">
+			<div className="ml-5 flex h-full items-center text-2xl">
+				<div className="flex h-[50px] w-[50px] cursor-pointer items-center justify-center rounded-full hover:bg-slate-200">
+					<i className="fa-solid fa-bars text-slate-500" />
+				</div>
+
+				<div className="ml-5">
+					<span className="text-2xl tracking-wider text-slate-500">Apotek</span>
+				</div>
+			</div>
+
+			<div className="mr-5">
+				<div className="flex h-[40px] w-[40px] cursor-pointer items-center justify-center rounded-full">
+					<img
+						className="h-full w-full rounded-full object-cover object-center"
+						src={profil}
+						alt="profil"
+					/>
+				</div>
+			</div>
+		</nav>
+	);
+}
+
+export default Navbar;
