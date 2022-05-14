74fe1d9aadedb7f531a2b6667929b442e1f4f351
Andria Pratama <andrilocong1st@gmail.com>
2022-05-15 01:20:35 +0800

    prepare supplier pages

---
+++ src/pages/suppliers/add-suppliers/index.jsx
@@ -0,0 +1,11 @@
+function AddSupplier() {
+	return (
+		<section className="add-supplier min-h-[80vh] w-full">
+			<div className="add-supplier__head mb-5">
+				<span className="text-2xl text-slate-600">Add Supplier</span>
+			</div>
+		</section>
+	);
+}
+
+export default AddSupplier;
---
+++ src/pages/suppliers/supplier-lists/index.jsx
@@ -0,0 +1,11 @@
+function SupplierList() {
+	return (
+		<section className="supplier-list min-h-[80vh] w-full">
+			<div className="supplier-list__head mb-5">
+				<span className="text-2xl text-slate-600">Supplier List</span>
+			</div>
+		</section>
+	);
+}
+
+export default SupplierList;
