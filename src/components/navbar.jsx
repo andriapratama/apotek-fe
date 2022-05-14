4d405507585fe93b88a5d38fff4e146c562ce687
Andria Pratama <andrilocong1st@gmail.com>
2022-05-15 01:19:04 +0800

    add display fixed

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,7 +2,7 @@ import profil from "../img/photo-1622548331053-105252394.jpeg";
 
 function Navbar({ setIsShowSidebar, isShowSidebar }) {
 	return (
-		<nav className="flex h-[60px] w-full items-center justify-between border-b border-slate-200 bg-white shadow-md shadow-slate-100">
+		<nav className="fixed z-50 flex h-[60px] w-full items-center justify-between border-b border-slate-200 bg-white shadow-md shadow-slate-100">
 			<div className="ml-5 flex h-full items-center text-2xl">
 				<div
 					className="flex h-[50px] w-[50px] cursor-pointer items-center justify-center rounded-full hover:bg-slate-200"
