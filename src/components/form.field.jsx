c82ad87138eea38ba2206cbc2ede44e7225ee31f
Andria Pratama <andrilocong1st@gmail.com>
2022-05-14 11:22:24 +0800

    remove red border

--- src/components/form.field.jsx
+++ src/components/form.field.jsx
@@ -1,12 +1,22 @@
-export const TextField = ({ ...props }) => {
+export const TextField = ({ errorMessage, touched, ...props }) => {
+	let isError = "";
+
+	if (touched === true) {
+		isError = errorMessage;
+	}
 	return (
-		<div className="flex w-full justify-center rounded-md border">
-			<input
-				className="w-[95%] py-1 text-slate-700 outline-none"
-				type="text"
-				{...props}
-				autoComplete="off"
-			/>
+		<div className="relative">
+			<div className={`flex h-auto w-full justify-center rounded-md border`}>
+				<input
+					className="w-[95%] py-1 text-slate-700 outline-none"
+					type="text"
+					{...props}
+					autoComplete="off"
+				/>
+			</div>
+			{isError ? (
+				<div className="absolute text-xs text-red-500">{errorMessage}</div>
+			) : null}
 		</div>
 	);
 };
