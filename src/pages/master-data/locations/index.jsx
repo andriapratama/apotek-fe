972c7e1e1f355842bce22e66622eb526207acdc4
Andria Pratama <andrilocong1st@gmail.com>
2022-05-18 14:19:48 +0800

    make location pages

---
+++ src/pages/master-data/locations/index.jsx
@@ -0,0 +1,128 @@
+import { IconButtonDelete, IconButtonEdit } from "../../../components/button";
+import { deleteLocationDataApi } from "../../../api/location.api";
+import LocationCreate from "./location.create";
+import LocationEdit from "./location.edit";
+import { Pagination } from "../../../components/pagination";
+import { DangerAlert } from "../../../components/alert";
+import { useLocationHook } from "./location.hook";
+
+function Location() {
+	const {
+		locationList,
+		locationValue,
+		setLocationValue,
+		showAllLocationData,
+		isDangerAlert,
+		setIsDangerAlert,
+		isShowUpdate,
+		setIsShowUpdate,
+		handleEdit,
+		errorsMessage,
+		setErrorsMessage,
+		formikStore,
+		formikUpdate,
+		totalPage,
+		handleNext,
+		handlePrev,
+		page,
+		handleSearch,
+	} = useLocationHook();
+
+	return (
+		<section className="location min-h-[80vh] w-full">
+			<DangerAlert
+				id={locationValue.locationId}
+				deleteDataApi={deleteLocationDataApi}
+				showAllData={showAllLocationData}
+				isDangerAlert={isDangerAlert}
+				setIsDangerAlert={setIsDangerAlert}
+			/>
+			<div className="location__head mb-5">
+				<span className="text-2xl text-slate-600">Data Location</span>
+			</div>
+
+			<div className="location__body mb-5 w-full rounded-lg border border-slate-200 bg-white shadow-lg shadow-slate-200">
+				<div className="p-5">
+					{isShowUpdate ? (
+						<LocationEdit
+							formik={formikUpdate}
+							errorsMessage={errorsMessage}
+							setErrorsMessage={setErrorsMessage}
+							setIsShowUpdate={setIsShowUpdate}
+							setLocationValue={setLocationValue}
+						/>
+					) : (
+						<LocationCreate
+							formik={formikStore}
+							errorsMessage={errorsMessage}
+							setErrorsMessage={setErrorsMessage}
+						/>
+					)}
+				</div>
+			</div>
+
+			<div className="location__content mb-5 w-full rounded-lg border border-slate-200 bg-white shadow-lg shadow-slate-200">
+				<div className="p-5">
+					<div className="mb-5 flex w-[350px] rounded-md border border-slate-200">
+						<input
+							className="ml-2 w-[300px] border-0 py-1 text-slate-700 outline-none"
+							placeholder="Search"
+							type="text"
+							onChange={(e) => handleSearch(e.target.value)}
+						/>
+						<div className="flex w-[50px] items-center justify-center border-l-2">
+							<i className="fa-solid fa-magnifying-glass text-slate-700" />
+						</div>
+					</div>
+
+					<table className="mb-5 w-full border-collapse text-center">
+						<thead>
+							<tr className="border-b-2 border-slate-200">
+								<th>No</th>
+								<th>Name</th>
+								<th>Action</th>
+							</tr>
+						</thead>
+						<tbody>
+							{locationList.map((location, index) => {
+								return (
+									<tr className="border-b border-slate-200" key={index}>
+										<td>{index + 1}</td>
+										<td className="capitalize">{location.name}</td>
+										<td className="flex justify-center py-2">
+											<div onClick={() => handleEdit(location)}>
+												<IconButtonEdit />
+											</div>
+											<div
+												onClick={() => {
+													setLocationValue({
+														locationId: location.location_id,
+														name: "",
+													});
+													setIsDangerAlert(true);
+												}}
+											>
+												<IconButtonDelete />
+											</div>
+										</td>
+									</tr>
+								);
+							})}
+						</tbody>
+					</table>
+
+					<div className="location__footer flex">
+						<Pagination
+							page={page}
+							totalPage={totalPage}
+							handleNext={handleNext}
+							handlePrev={handlePrev}
+						/>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+}
+
+export default Location;
---
+++ src/pages/master-data/locations/location.hook.js
@@ -0,0 +1,154 @@
+import { useState, useEffect } from "react";
+import { useFormik } from "formik";
+import {
+	findAllLocationDataApi,
+	findLocationDataByNameApi,
+	updateLocationDataApi,
+	storeLocationDataApi,
+} from "../../../api/location.api";
+import { validation } from "./location.validation";
+import { useDispatch } from "react-redux";
+import {
+	nextPage,
+	prevPage,
+} from "../../../stores/reducers/pagination.reducer";
+import { store } from "../../../stores";
+
+export const useLocationHook = () => {
+	const dispatch = useDispatch();
+	const pageRedux = store.getState().pagination.page;
+	const [locationList, setLocationList] = useState([]);
+	const [isShowUpdate, setIsShowUpdate] = useState(false);
+	const [isDangerAlert, setIsDangerAlert] = useState(false);
+	const [errorsMessage, setErrorsMessage] = useState("");
+	const [totalPage, setTotalPage] = useState(0);
+	const [page, setPage] = useState(pageRedux);
+	const [locationValue, setLocationValue] = useState({
+		locationId: "",
+		name: "",
+	});
+
+	//Store Location Data Start
+	const storeData = async (value) => {
+		try {
+			await storeLocationDataApi(value.name);
+
+			formikStore.resetForm();
+			showAllLocationData();
+		} catch (error) {
+			setErrorsMessage(error.response.data.message);
+		}
+	};
+
+	const formikStore = useFormik({
+		initialValues: {
+			name: "",
+		},
+		validationSchema: validation,
+		onSubmit: (value) => {
+			storeData(value);
+		},
+	});
+	//Store Location Data End
+
+	//Show All Location Data Start
+	const showAllLocationData = async () => {
+		const location = await findAllLocationDataApi(page);
+
+		setLocationList(location.data.data.location.rows);
+		setTotalPage(location.data.data.pagination.totalPage);
+	};
+	//Show All Location Data End
+
+	useEffect(() => {
+		showAllLocationData();
+		// eslint-disable-next-line
+	}, []);
+
+	//Edit Location Data Start
+	const handleEdit = (location) => {
+		setIsShowUpdate(true);
+		setLocationValue({
+			locationId: location.location_id,
+			name: location.name,
+		});
+	};
+
+	const updateLocationData = async (values) => {
+		try {
+			await updateLocationDataApi(locationValue.locationId, values.name);
+
+			showAllLocationData();
+			setIsShowUpdate(false);
+			setLocationValue({
+				locationId: "",
+				name: "",
+			});
+		} catch (error) {
+			setErrorsMessage(error.response.data.message);
+		}
+	};
+
+	const formikUpdate = useFormik({
+		initialValues: {
+			name: locationValue.name,
+		},
+		enableReinitialize: true,
+		validationSchema: validation,
+		onSubmit: (values) => {
+			updateLocationData(values);
+		},
+	});
+	//Edit Location Data End
+
+	//Pagination Start
+	const handleNext = async () => {
+		dispatch(nextPage());
+		setPage(pageRedux + 1);
+		const location = await findAllLocationDataApi(page + 1);
+
+		setLocationList(location.data.data.location.rows);
+	};
+
+	const handlePrev = async () => {
+		dispatch(prevPage());
+		setPage(pageRedux - 1);
+		const location = await findAllLocationDataApi(page - 1);
+
+		setLocationList(location.data.data.location.rows);
+	};
+	//Pagination End
+
+	//Feature Search Start
+	const handleSearch = async (search) => {
+		if (search === "") {
+			showAllLocationData();
+		} else {
+			const location = await findLocationDataByNameApi(search);
+
+			setLocationList(location.data.data.location);
+		}
+	};
+	//Feature Search End
+
+	return {
+		locationList,
+		locationValue,
+		setLocationValue,
+		showAllLocationData,
+		isDangerAlert,
+		setIsDangerAlert,
+		isShowUpdate,
+		setIsShowUpdate,
+		handleEdit,
+		errorsMessage,
+		setErrorsMessage,
+		formikStore,
+		formikUpdate,
+		totalPage,
+		handleNext,
+		handlePrev,
+		page,
+		handleSearch,
+	};
+};
