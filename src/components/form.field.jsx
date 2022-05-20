564de71b8d1a3592e10d413bc1788dd40eef74da
Andria Pratama <andrilocong1st@gmail.com>
2022-05-20 19:51:13 +0800

    make number field

--- src/components/form.field.jsx
+++ src/components/form.field.jsx
@@ -13,7 +13,7 @@ export const TextField = ({ errorMessage, touched, ...props }) => {
 				className={`flex h-auto w-full justify-center rounded-md border border-slate-400`}
 			>
 				<input
-					className="w-[95%] py-[4px] text-slate-700 outline-none"
+					className="w-[96%] py-[4px] text-slate-700 outline-none"
 					type="text"
 					{...props}
 					autoComplete="off"
@@ -237,6 +237,48 @@ export const CustomSelectMultipleField = ({
 						</div>
 					</>
 				) : null}
+			</div>
+			{isError ? (
+				<div className="absolute text-xs text-red-500">{errorMessage}</div>
+			) : null}
+		</div>
+	);
+};
+
+export const NumberField = ({
+	name,
+	errorMessage,
+	touched,
+	setFieldValue,
+	...props
+}) => {
+	const handleChange = (e) => {
+		const regex = /^[0-9\b]+$/;
+
+		if (e.target.value === "" || regex.test(e.target.value)) {
+			setFieldValue(`${name}`, e.target.value);
+		}
+	};
+
+	let isError = "";
+
+	if (touched === true) {
+		isError = errorMessage;
+	}
+
+	return (
+		<div className="relative">
+			<div
+				className={`flex h-auto w-full justify-center rounded-md border border-slate-400`}
+			>
+				<input
+					name={name}
+					className="w-[96%] py-[4px] text-slate-700 outline-none"
+					type="text"
+					{...props}
+					autoComplete="off"
+					onChange={(e) => handleChange(e)}
+				/>
 			</div>
 			{isError ? (
 				<div className="absolute text-xs text-red-500">{errorMessage}</div>
