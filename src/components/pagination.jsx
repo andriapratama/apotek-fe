52899737c3f2f739eec59a1b36076625c35654f5
Andria Pratama <andrilocong1st@gmail.com>
2022-05-21 10:29:49 +0800

    add set page

--- src/components/pagination.jsx
+++ src/components/pagination.jsx
@@ -1,9 +1,38 @@
-export const Pagination = ({ page, totalPage, handleNext, handlePrev }) => {
+import { store } from "../stores";
+
+export const Pagination = ({
+	page,
+	totalPage,
+	handleSetPage,
+	handleNext,
+	handlePrev,
+}) => {
+	const pageRedux = store.getState().pagination.page;
+
+	const rangePage = () => {
+		let page = [];
+		for (let i = 1; i <= totalPage; i++) {
+			page.push(
+				<div
+					className={`flex h-8 w-8 cursor-pointer items-center justify-center border border-slate-400 text-slate-700 ${
+						i === pageRedux ? "bg-slate-300" : "bg-transparent"
+					}`}
+					key={i}
+					onClick={() => {
+						handleSetPage(i);
+					}}
+				>
+					<span>{i}</span>
+				</div>
+			);
+		}
+		return page;
+	};
 	return (
 		<>
 			{page <= 1 ? null : (
 				<div
-					className="flex h-8 w-8 cursor-pointer items-center justify-center rounded-l-md border text-slate-700 hover:bg-slate-200"
+					className="flex h-8 w-8 cursor-pointer items-center justify-center rounded-l-md border border-slate-400 text-slate-700 hover:bg-slate-200"
 					onClick={() => {
 						handlePrev();
 					}}
@@ -12,13 +41,11 @@ export const Pagination = ({ page, totalPage, handleNext, handlePrev }) => {
 				</div>
 			)}
 
-			<div className="flex h-8 w-8 items-center justify-center border text-slate-700">
-				<span>{page}</span>
-			</div>
+			<div className="flex">{rangePage()}</div>
 
 			{page >= totalPage ? null : (
 				<div
-					className="flex h-8 w-8 cursor-pointer items-center justify-center rounded-r-md border text-slate-700 hover:bg-slate-200"
+					className="flex h-8 w-8 cursor-pointer items-center justify-center rounded-r-md border border-slate-400 text-slate-700 hover:bg-slate-200"
 					onClick={() => {
 						handleNext();
 					}}
