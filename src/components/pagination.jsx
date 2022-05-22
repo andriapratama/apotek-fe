c9306b47269cc5c8c1f58f1463f61196f87db8cd
Andria Pratama <andrilocong1st@gmail.com>
2022-05-23 00:35:24 +0800

    add pagination stock and price for detail product

--- src/components/pagination.jsx
+++ src/components/pagination.jsx
@@ -56,3 +56,117 @@ export const Pagination = ({
 		</>
 	);
 };
+
+export const PaginationStock = ({
+	page,
+	totalPage,
+	handleSetPage,
+	handleNext,
+	handlePrev,
+}) => {
+	const pageRedux = store.getState().paginationStock.page;
+
+	const rangePage = () => {
+		let page = [];
+		for (let i = 1; i <= totalPage; i++) {
+			page.push(
+				<div
+					className={`flex h-8 w-8 cursor-pointer items-center justify-center border border-slate-400 text-slate-700 ${
+						i === pageRedux ? "bg-slate-300" : "bg-transparent"
+					}`}
+					key={i}
+					onClick={() => {
+						handleSetPage(i);
+					}}
+				>
+					<span>{i}</span>
+				</div>
+			);
+		}
+		return page;
+	};
+	return (
+		<>
+			{page <= 1 ? null : (
+				<div
+					className="flex h-8 w-8 cursor-pointer items-center justify-center rounded-l-md border border-slate-400 text-slate-700 hover:bg-slate-200"
+					onClick={() => {
+						handlePrev();
+					}}
+				>
+					<i className="fa-solid fa-angle-left text-slate-700" />
+				</div>
+			)}
+
+			<div className="flex">{rangePage()}</div>
+
+			{page >= totalPage ? null : (
+				<div
+					className="flex h-8 w-8 cursor-pointer items-center justify-center rounded-r-md border border-slate-400 text-slate-700 hover:bg-slate-200"
+					onClick={() => {
+						handleNext();
+					}}
+				>
+					<i className="fa-solid fa-angle-right text-slate-700" />
+				</div>
+			)}
+		</>
+	);
+};
+
+export const PaginationPrice = ({
+	page,
+	totalPage,
+	handleSetPage,
+	handleNext,
+	handlePrev,
+}) => {
+	const pageRedux = store.getState().paginationPrice.page;
+
+	const rangePage = () => {
+		let page = [];
+		for (let i = 1; i <= totalPage; i++) {
+			page.push(
+				<div
+					className={`flex h-8 w-8 cursor-pointer items-center justify-center border border-slate-400 text-slate-700 ${
+						i === pageRedux ? "bg-slate-300" : "bg-transparent"
+					}`}
+					key={i}
+					onClick={() => {
+						handleSetPage(i);
+					}}
+				>
+					<span>{i}</span>
+				</div>
+			);
+		}
+		return page;
+	};
+	return (
+		<>
+			{page <= 1 ? null : (
+				<div
+					className="flex h-8 w-8 cursor-pointer items-center justify-center rounded-l-md border border-slate-400 text-slate-700 hover:bg-slate-200"
+					onClick={() => {
+						handlePrev();
+					}}
+				>
+					<i className="fa-solid fa-angle-left text-slate-700" />
+				</div>
+			)}
+
+			<div className="flex">{rangePage()}</div>
+
+			{page >= totalPage ? null : (
+				<div
+					className="flex h-8 w-8 cursor-pointer items-center justify-center rounded-r-md border border-slate-400 text-slate-700 hover:bg-slate-200"
+					onClick={() => {
+						handleNext();
+					}}
+				>
+					<i className="fa-solid fa-angle-right text-slate-700" />
+				</div>
+			)}
+		</>
+	);
+};
