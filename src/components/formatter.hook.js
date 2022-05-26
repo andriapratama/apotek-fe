53ba5d55ff98367096ab8f004f59edb98e21fe88
Andria Pratama <andrilocong1st@gmail.com>
2022-05-26 21:50:11 +0800

    make formatter stock and IDR

---
+++ src/components/formatter.hook.js
@@ -0,0 +1,25 @@
+export const useFormatterHook = () => {
+	//Formatter Stock Start
+	const formatterStock = (value) => {
+		const reverse = value.toString().split("").reverse().join("");
+		const modify = reverse.match(/\d{1,3}/g);
+		const format = modify.join(".").split("").reverse().join("");
+
+		return format;
+	};
+	//Formatter Stock End
+
+	//Formatter IDR Start
+	const formatterIDR = (value) => {
+		const format = value.toString().split("").reverse().join("");
+		const convert = format.match(/\d{1,3}/g);
+		const IDR = "Rp " + convert.join(".").split("").reverse().join("");
+
+		return IDR;
+	};
+	//Formatter IDR End
+
+	return { formatterIDR, formatterStock };
+};
+
+export default useFormatterHook;
