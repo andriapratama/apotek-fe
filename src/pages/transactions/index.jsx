c599ecdc152409f15fe3b7a6c310920f86e54381
Andria Pratama <andrilocong1st@gmail.com>
2022-05-27 15:04:35 +0800

    show data transaction and pagination

--- src/pages/transactions/index.jsx
+++ src/pages/transactions/index.jsx
@@ -1,7 +1,26 @@
+import moment from "moment";
+import "moment/locale/id";
+import { useNavigate } from "react-router-dom";
 import { IconButtonDetail } from "../../components/button";
+import { Pagination } from "../../components/pagination";
 import { SearchFeature } from "../../components/search";
+import useTransactionHook from "./transaction.hook";
+import useFormatterHook from "../../components/formatter.hook";
 
 function TransactionList() {
+	const naviagte = useNavigate();
+
+	const {
+		transactionList,
+		page,
+		totalPage,
+		handleNext,
+		handlePrev,
+		handleSetPage,
+	} = useTransactionHook();
+
+	const { formatterIDR } = useFormatterHook();
+
 	return (
 		<section className="transaction-list min-h-[80vh] w-full">
 			<div className="transaction-list__head mb-5">
@@ -10,7 +29,7 @@ function TransactionList() {
 
 			<div className="transaction-list__body mb-5 w-full rounded-lg border border-slate-200 bg-white shadow-lg shadow-slate-400">
 				<div className="p-5">
-					<SearchFeature placeholder="Search name product" />
+					<SearchFeature placeholder="Search Transaction Code" />
 
 					<table className="mb-5 w-full border-collapse text-center text-slate-600">
 						<thead>
@@ -23,35 +42,43 @@ function TransactionList() {
 							</tr>
 						</thead>
 						<tbody>
-							<tr className="border-b border-slate-400">
-								<td className="py-1">12/12/2012</td>
-								<td className="py-1">TS20121212001</td>
-								<td className="py-1">-</td>
-								<td className="py-1">Rp 50.000</td>
-								<td className="flex justify-center py-1">
-									<IconButtonDetail />
-								</td>
-							</tr>
-							<tr className="border-b border-slate-400">
-								<td className="py-1">12/12/2012</td>
-								<td className="py-1">TS20121212002</td>
-								<td className="py-1">-</td>
-								<td className="py-1">Rp 50.000</td>
-								<td className="flex justify-center py-1">
-									<IconButtonDetail />
-								</td>
-							</tr>
-							<tr className="border-b border-slate-400">
-								<td className="py-1">12/12/2012</td>
-								<td className="py-1">TS20121212003</td>
-								<td className="py-1">-</td>
-								<td className="py-1">Rp 50.000</td>
-								<td className="flex justify-center py-1">
-									<IconButtonDetail />
-								</td>
-							</tr>
+							{transactionList.map((transaction, index) => {
+								return (
+									<tr className="border-b border-slate-400" key={index}>
+										<td className="py-1">
+											{moment(transaction.createdAt).local("id").format("L")}
+										</td>
+										<td className="py-1">{transaction.transaction_id}</td>
+										<td className="py-1">-</td>
+										<td className="py-1">
+											{formatterIDR(transaction.grand_total)}
+										</td>
+										<td className="flex justify-center py-1">
+											<div
+												onClick={() => {
+													naviagte(
+														`/transaction/detail/${transaction.transaction_id}`
+													);
+												}}
+											>
+												<IconButtonDetail />
+											</div>
+										</td>
+									</tr>
+								);
+							})}
 						</tbody>
 					</table>
+
+					<div className="flex">
+						<Pagination
+							page={page}
+							totalPage={totalPage}
+							handleSetPage={handleSetPage}
+							handleNext={handleNext}
+							handlePrev={handlePrev}
+						/>
+					</div>
 				</div>
 			</div>
 		</section>
