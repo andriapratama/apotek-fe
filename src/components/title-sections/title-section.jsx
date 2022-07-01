99f061d0c45dec94fdb2c1ca63723c65f66161d9
Andria Pratama <andrilocong1st@gmail.com>
2022-07-01 14:15:04 +0800

    create title section component

---
+++ src/components/title-sections/title-section.jsx
@@ -0,0 +1,9 @@
+const TitleSection = ({ children }) => {
+	return (
+		<div className="mb-5">
+			<span className="text-2xl text-slate-600">{children}</span>
+		</div>
+	);
+};
+
+export default TitleSection;
