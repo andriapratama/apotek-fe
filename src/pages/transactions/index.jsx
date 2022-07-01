67aa121c01f828cc01d0c646452b30775348f2fc
Andria Pratama <andrilocong1st@gmail.com>
2022-07-01 15:36:50 +0800

    modify component

--- src/pages/transactions/index.jsx
+++ src/pages/transactions/index.jsx
@@ -6,6 +6,8 @@ import { Pagination } from "../../components/pagination";
 import { SearchFeature } from "../../components/search";
 import useTransactionHook from "./transaction.hook";
 import useFormatterHook from "../../components/formatter.hook";
+import TitleSection from "../../components/title-sections/title-section";
+import CardContent from "../../components/cards/card.content";
 
 function TransactionList() {
 	const naviagte = useNavigate();
@@ -23,64 +25,60 @@ function TransactionList() {
 
 	return (
 		<section className="transaction-list min-h-[80vh] w-full">
-			<div className="transaction-list__head mb-5">
-				<span className="text-2xl text-slate-600">Transaction List</span>
-			</div>
+			<TitleSection>Transaction List</TitleSection>
 
-			<div className="transaction-list__body mb-5 w-full rounded-lg border border-slate-200 bg-white shadow-lg shadow-slate-400">
-				<div className="p-5">
-					<SearchFeature placeholder="Search Transaction Code" />
+			<CardContent>
+				<SearchFeature placeholder="Search Transaction Code" />
 
-					<table className="mb-5 w-full border-collapse text-center text-slate-600">
-						<thead>
-							<tr className="border-b-2 border-slate-400">
-								<th>Date</th>
-								<th>Code</th>
-								<th>Cashier</th>
-								<th>Total</th>
-								<th>Action</th>
-							</tr>
-						</thead>
-						<tbody>
-							{transactionList.map((transaction, index) => {
-								return (
-									<tr className="border-b border-slate-400" key={index}>
-										<td className="py-1">
-											{moment(transaction.createdAt).local("id").format("L")}
-										</td>
-										<td className="py-1">{transaction.transaction_id}</td>
-										<td className="py-1">-</td>
-										<td className="py-1">
-											{formatterIDR(transaction.grand_total)}
-										</td>
-										<td className="flex justify-center py-1">
-											<div
-												onClick={() => {
-													naviagte(
-														`/transaction/detail/${transaction.transaction_id}`
-													);
-												}}
-											>
-												<IconButtonDetail />
-											</div>
-										</td>
-									</tr>
-								);
-							})}
-						</tbody>
-					</table>
+				<table className="table">
+					<thead>
+						<tr className="tr-head">
+							<th>Date</th>
+							<th>Code</th>
+							<th>Cashier</th>
+							<th>Total</th>
+							<th>Action</th>
+						</tr>
+					</thead>
+					<tbody>
+						{transactionList.map((transaction, index) => {
+							return (
+								<tr className="tr-body" key={index}>
+									<td className="py-1">
+										{moment(transaction.createdAt).local("id").format("L")}
+									</td>
+									<td className="py-1">{transaction.transaction_id}</td>
+									<td className="py-1">-</td>
+									<td className="py-1">
+										{formatterIDR(transaction.grand_total)}
+									</td>
+									<td className="flex justify-center py-1">
+										<div
+											onClick={() => {
+												naviagte(
+													`/transaction/detail/${transaction.transaction_id}`
+												);
+											}}
+										>
+											<IconButtonDetail />
+										</div>
+									</td>
+								</tr>
+							);
+						})}
+					</tbody>
+				</table>
 
-					<div className="flex">
-						<Pagination
-							page={page}
-							totalPage={totalPage}
-							handleSetPage={handleSetPage}
-							handleNext={handleNext}
-							handlePrev={handlePrev}
-						/>
-					</div>
+				<div className="flex">
+					<Pagination
+						page={page}
+						totalPage={totalPage}
+						handleSetPage={handleSetPage}
+						handleNext={handleNext}
+						handlePrev={handlePrev}
+					/>
 				</div>
-			</div>
+			</CardContent>
 		</section>
 	);
 }
--- src/pages/transactions/transaction-details/index.jsx
+++ src/pages/transactions/transaction-details/index.jsx
@@ -1,6 +1,8 @@
 import moment from "moment";
 import { useTransactionDetailHook } from "./transaction-detail.hook";
 import { useFormatterHook } from "../../../components/formatter.hook";
+import TitleSection from "../../../components/title-sections/title-section";
+import CardContent from "../../../components/cards/card.content";
 
 function TransactionDetail() {
 	const { transactionValue } = useTransactionDetailHook();
@@ -12,94 +14,90 @@ function TransactionDetail() {
 
 	return (
 		<section className="transaction-detail w-full">
-			<div className="transaction-detail__title mb-5">
-				<span className="text-2xl text-slate-600">Transaction Detail</span>
-			</div>
+			<TitleSection>Transaction Detail</TitleSection>
 
-			<div className="card mb-5">
-				<div className="p-5">
-					<div className="cashier-head mb-5 border-b-2 border-sky-500 pb-5">
-						<div className="mb-5 flex justify-between">
-							<div className="flex w-[48%] items-center justify-between">
-								<p className="text-slate-600">Date/Time</p>
-								<div className="w-[80%] rounded-lg border border-slate-400 bg-slate-200 py-1 px-2 text-slate-600">
-									<span>
-										{date} / {time}
-									</span>
-								</div>
-							</div>
-
-							<div className="flex w-[48%] items-center justify-between">
-								<p className="text-slate-600">Cashier Name</p>
-								<div className="w-[75%] rounded-lg border border-slate-400 bg-slate-200 py-1 px-2 text-slate-600">
-									{transactionValue.cashier}
-								</div>
+			<CardContent>
+				<div className="cashier-head mb-5 border-b-2 border-sky-500 pb-5">
+					<div className="mb-5 flex justify-between">
+						<div className="flex w-[48%] items-center justify-between">
+							<p className="text-slate-600">Date/Time</p>
+							<div className="w-[80%] rounded-lg border border-slate-400 bg-slate-200 py-1 px-2 text-slate-600">
+								<span>
+									{date} / {time}
+								</span>
 							</div>
 						</div>
+
 						<div className="flex w-[48%] items-center justify-between">
-							<p className="text-slate-600">Transaction Code</p>
-							<div className="w-[71%] rounded-lg border border-slate-400 bg-slate-200 py-1 px-2 text-slate-600">
-								{transactionValue.transactionId}
+							<p className="text-slate-600">Cashier Name</p>
+							<div className="w-[75%] rounded-lg border border-slate-400 bg-slate-200 py-1 px-2 text-slate-600">
+								{transactionValue.cashier}
 							</div>
 						</div>
 					</div>
-
-					<table className="table">
-						<thead className="text-left">
-							<tr className="border-b-2 border-slate-400">
-								<th className="pl-2">Code</th>
-								<th>Name</th>
-								<th>Qty</th>
-								<th>Price</th>
-								<th>Disc</th>
-								<th>Total Disc</th>
-								<th>Sub Total</th>
-							</tr>
-						</thead>
-						<tbody className="text-left">
-							{transactionValue.detailList.map((detail, index) => {
-								return (
-									<tr className="border-b border-slate-400" key={index}>
-										<td className="py-2 pl-2">{detail.product_id}</td>
-										<td className="py-2">{detail.name}</td>
-										<td className="py-2">
-											<span>
-												{detail.quantity} {detail.product.unit.name}
-											</span>
-										</td>
-										<td className="py-2">
-											{formatterIDR(detail.product.selling_price)}
-										</td>
-										<td className="py-2">
-											<span>{detail.discount}%</span>
-										</td>
-										<td className="py-2">
-											{formatterIDR(detail.total_discount)}
-										</td>
-										<td className="py-2">{formatterIDR(detail.sub_total)}</td>
-									</tr>
-								);
-							})}
-						</tbody>
-						<tbody>
-							<tr>
-								<td
-									className="pr-10 pt-5 text-right text-lg font-bold"
-									colSpan={6}
-								>
-									Total
-								</td>
-								<td className="pt-5 text-lg" colSpan={2}>
-									{formatterIDR(transactionValue.total)}
-								</td>
-							</tr>
-						</tbody>
-					</table>
+					<div className="flex w-[48%] items-center justify-between">
+						<p className="text-slate-600">Transaction Code</p>
+						<div className="w-[71%] rounded-lg border border-slate-400 bg-slate-200 py-1 px-2 text-slate-600">
+							{transactionValue.transactionId}
+						</div>
+					</div>
 				</div>
-			</div>
 
-			<div className="card">
-				<div className="p-5 text-slate-600">
+				<table className="table">
+					<thead className="text-left">
+						<tr className="tr-head">
+							<th className="pl-2">Code</th>
+							<th>Name</th>
+							<th>Qty</th>
+							<th>Price</th>
+							<th>Disc</th>
+							<th>Total Disc</th>
+							<th>Sub Total</th>
+						</tr>
+					</thead>
+					<tbody className="text-left">
+						{transactionValue.detailList.map((detail, index) => {
+							return (
+								<tr className="tr-body" key={index}>
+									<td className="py-2 pl-2">{detail.product_id}</td>
+									<td className="py-2">{detail.name}</td>
+									<td className="py-2">
+										<span>
+											{detail.quantity} {detail.product.unit.name}
+										</span>
+									</td>
+									<td className="py-2">
+										{formatterIDR(detail.product.selling_price)}
+									</td>
+									<td className="py-2">
+										<span>{detail.discount}%</span>
+									</td>
+									<td className="py-2">
+										{formatterIDR(detail.total_discount)}
+									</td>
+									<td className="py-2">{formatterIDR(detail.sub_total)}</td>
+								</tr>
+							);
+						})}
+					</tbody>
+					<tbody>
+						<tr>
+							<td
+								className="pr-10 pt-5 text-right text-lg font-bold"
+								colSpan={6}
+							>
+								Total
+							</td>
+							<td className="pt-5 text-lg" colSpan={2}>
+								{formatterIDR(transactionValue.total)}
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</CardContent>
+
+			<CardContent>
+				<div className="text-slate-600">
 					<div className="mb-5 text-lg tracking-wide">Payment</div>
 
 					<div className="mb-2 flex items-center">
@@ -143,7 +141,7 @@ function TransactionDetail() {
 						</div>
 					</div>
 				</div>
-			</div>
+			</CardContent>
 		</section>
 	);
 }
