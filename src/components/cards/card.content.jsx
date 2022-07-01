6a0ad487bf44cb4fe4a52d892b0456cfc9b00444
Andria Pratama <andrilocong1st@gmail.com>
2022-07-01 14:10:37 +0800

    create card component

---
+++ src/components/cards/card.content.jsx
@@ -0,0 +1,5 @@
+const CardContent = ({ children }) => {
+	return <div className="card">{children}</div>;
+};
+
+export default CardContent;
---
+++ src/components/cards/card.titles.jsx
@@ -0,0 +1,9 @@
+const CardTitle = ({ children }) => {
+	return (
+		<div className="mb-5">
+			<p className="text-xl text-slate-600">{children}</p>
+		</div>
+	);
+};
+
+export default CardTitle;
